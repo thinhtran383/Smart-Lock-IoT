f5c60f0649207c18c0a0396b695b8aebb4229055
ThinhTran <thinhtran3803@gmail.com>
2023-12-07 12:25:54 +0700

    init

---
+++ src/main/java/com/group/smartlockiot/SmartLockIoTApplication.java
@@ -0,0 +1,13 @@
+package com.group.smartlockiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLockIoTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLockIoTApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group/smartlockiot/SmartLockIoTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.smartlockiot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLockIoTApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
